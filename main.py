27411af861c643378dab1b26d2503159efd78251
Parvanitis15 <pearvan14@gmail.com>
2023-10-20 14:57:00 +0300

    Complete EDA, basic_cleaning and update dependencies

--- main.py
+++ main.py
@@ -28,14 +28,9 @@ def go(config: DictConfig):
     os.environ["WANDB_PROJECT"] = config["main"]["project_name"]
     os.environ["WANDB_RUN_GROUP"] = config["main"]["experiment_name"]
 
-    print(config["main"]["experiment_name"])
-    print(config["main"]["project_name"])
-
     # Steps to execute
     steps_par = config['main']['steps']
     active_steps = steps_par.split(",") if steps_par != "all" else _steps
-
-    print(active_steps)
 
     # Move to a temporary directory
     with tempfile.TemporaryDirectory() as tmp_dir:
@@ -55,10 +50,18 @@ def go(config: DictConfig):
             )
 
         if "basic_cleaning" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
+                "main",
+                parameters={
+                    "input_artifact": "sample.csv:latest",
+                    "output_artifact": "clean_sample.csv",
+                    "output_type": "clean_sample",
+                    "output_description": "Data with outliers and null values removed",
+                    "min_price": config['etl']['min_price'],
+                    "max_price": config['etl']['max_price']
+                },
+            )
 
         if "data_check" in active_steps:
             ##################
--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 """
-Performs basic cleaning on the data and save the results in Weights & Biases
+Download from W&B the raw dataset and apply some basic data cleaning, exporting the result to a new artifact
 """
 import argparse
 import logging
+
+import pandas as pd
 import wandb
 
 
@@ -16,38 +18,83 @@ def go(args):
     run = wandb.init(job_type="basic_cleaning")
     run.config.update(args)
 
-    # Download input artifact. This will also log that this script is using this
-    # particular version of the artifact
-    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+    # 1. Download artifact
+    logger.info("Downloading artifact")
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
+    logger.info("Downloaded artifact")
 
-    ######################
-    # YOUR CODE HERE     #
-    ######################
+    # 3. Clean data
+    logger.info("Cleaning data")
+    df = pd.read_csv(artifact_local_path)
+    df = df.dropna()
+    df = df[df.price.between(args.min_price, args.max_price)]
+    logger.info("Cleaned data")
+
+    # 4. Upload artifact
+    logger.info("Uploading artifact")
+    artifact = wandb.Artifact(
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description,
+    )
+    logger.info("Uploaded artifact")
+
+    # 5. Log output artifact
+    logger.info("Logging artifact")
+    artifact.add_file(artifact_local_path)
+    run.log_artifact(artifact)
+    logger.info("Logged artifact")
+
+    # 6. Finish run
+    logger.info("Finishing run")
+    run.finish()
+    logger.info("Finished run")
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="This steps cleans the data")
+    parser = argparse.ArgumentParser(description="A very basic data cleaning")
 
 
     parser.add_argument(
-        "--parameter1", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        "--input_artifact", 
+        type=str,
+        help="The input artifact containing the raw data",
         required=True
     )
 
     parser.add_argument(
-        "--parameter2", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        "--output_artifact", 
+        type=str,
+        help="The output artifact containing the cleaned data",
         required=True
     )
 
     parser.add_argument(
-        "--parameter3", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        "--output_type", 
+        type=str,
+        help="The output artifact type",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_description", 
+        type=str,
+        help="The output artifact description",
+        required=True
+    )
+
+    parser.add_argument(
+        "--min_price", 
+        type=float,
+        help="The minimum price to consider",
+        required=True
+    )
+
+    parser.add_argument(
+        "--max_price", 
+        type=float,
+        help="The maximum price to consider",
         required=True
     )
 
